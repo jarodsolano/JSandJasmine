96d4bc7f5e07aaf3e08bc9fc5796882271a47497
jarodsolano <jarod.solano@gmail.com>
2015-07-30 23:38:53 -0600

    Initial commit

---
+++ spec/HelloWorldSpec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Jarod on 7/30/2015.
+ */
+
+describe("Hello world", function(){
+    it("says hello", function() {
+        expect(helloWorld()).toEqual("Hello world");
+    });
+});
---
+++ testFunctions.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Jarod on 7/30/2015.
+ */
+
+function helloWorld() {
+    return "Hello world!";
+}
